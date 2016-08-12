931a271e56134dcc35029bf75260513b60884f6c
Jinwithyoo <jinwithyoo@gmail.com>
2016-08-12 18:38:37 +0900

    Update __init__.py

--- hangulize/__init__.py
+++ hangulize/__init__.py
@@ -124,4 +124,4 @@ for name in ['.models', '.normalization', '.processing']:
     module = importlib.import_module(name, __name__)
     __all__.extend(module.__all__)
     for attr in module.__all__:
-        locals()[attr] = getattr(module, 'attr')
+        locals()[attr] = getattr(module, attr)
